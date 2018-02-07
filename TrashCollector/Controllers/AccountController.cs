5de5fc0b44f041e78673229414404def777d4e59
Curtis Hansen <curtishansen14@gmail.com>
2018-02-07 10:31:30 -0600

    created customer model

--- TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/Controllers/AccountController.cs
@@ -158,6 +158,7 @@ namespace TrashCollector.Controllers
             foreach (var role in RoleManager.Roles)
                 list.Add(new SelectListItem() { Value = role.Name, Text = role.Name });
             ViewBag.Roles = list;
+            list.RemoveAt(2);
             return View();
         }
 
---
+++ TrashCollector/Models/CustomerModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashCollector.Models
+{
+    public class CustomerModel
+    {
+        public string ID { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Address")]
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "Collection Day")]
+        [Required]
+        public DayOfWeek CollectionDay { get; set; }
+
+    }
+}
